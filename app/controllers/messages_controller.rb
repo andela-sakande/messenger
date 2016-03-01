39c4e9bfa6a8afcb2871b097a4569a5e534d4af8
andela-sakande <surajudeen.akande@andela.com>
2016-03-01 15:00:42 +0100

    Create new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,18 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
   def new
-
+    @message = Message.new
+  end
+  def create
+  @message = Message.new(message_params)
+  if @message.save
+    redirect_to '/messages'
+  else
+    render 'new'
+  end
+end
+  private
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome' => 'pages#home'
   get 'messages' => 'messages#index'
   get 'messages/new' => 'messages#new'
+  post 'messages' => 'messages#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
